14d606175ed1eb2424f308bf443da72f00925a8b
peke-pon <toyo718@gmail.com>
2020-03-04 19:14:38 +0900

    slot

---
+++ 15.slot/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <h2>15.slot</h2>
+    <slot-child>
+      <h3>デフォルトslot</h3>
+      <p>{{ message }}</p>
+      <template v-slot:name>
+        <h3>名前付きスロット</h3>
+        <p>templateタグに<strong>v-slot:slot名</strong>で指定したスロットへ渡す</p>
+        <hr />
+      </template>
+      <template v-slot:[fall]>
+      </template>
+      <p>タグ内の<strong>名前のついていない要素</strong>はまとめてv-slot:defaltへ渡される</p>
+      <hr />
+      <template #props="slotProps">
+        <h3>スロットプロパティ</h3>
+        <p>{{ slotProps.message2.msg2 }}</p>
+        <p>{{ slotProps }}</p>
+      </template>
+    </slot-child>
+    <hr />
+  </div>
+</template>
+
+<script>
+import SlotChild from "@/components/SlotChild.vue";
+
+export default {
+  name: "App",
+  components: {
+    SlotChild
+  },
+  data() {
+    return {
+      message: "HTML要素を子コンポーネントへ渡し、子要素のslotで受け取る",
+      fall: 'fallback'
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #fbfbfc;
+  color: #454659;
+  height: 100vh;
+}
+
+h2 {
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+
+a {
+  text-decoration: none;
+}
+
+p {
+  padding: 0 1em;
+}
+
+ul {
+  list-style: none;
+  margin-left: -1.5em;
+}
+
+li {
+  font-weight: bold;
+  margin-bottom: 0.5em;
+}
+
+hr {
+  margin: 30px 0;
+}
+
+button {
+  margin-left: 2em;
+}
+
+td {
+  padding: 0 1em;
+}
+
+strong {
+  padding: 0.5em;
+}
+
+.price {
+  font-size: 2.4em;
+  font-weight: bold;
+  color: rgb(255, 84, 16);
+}
+</style>
---
+++ 15.slot/src/components/SlotChild.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <slot></slot>
+    <slot name="name"></slot>
+    <slot name="fallback">
+      <h3>フォールバックコンテンツ</h3>
+      <p>親要素のslot内に要素がない場合は表示される</p>
+      <hr>
+    </slot>
+    <slot name="props" :message2="message2"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SlotChild",
+  data() {
+    return {
+      message: "子コンポーネントの変数で書き換えはされない",
+      message2: {
+        msg2: "slotタグの属性を指定することでデータを渡す"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
